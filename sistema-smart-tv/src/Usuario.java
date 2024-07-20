fa62d197544101ea44a1f034929f77108be21100
paulokalleby <paulo.devweb@gmail.com>
2024-07-20 15:56:27 -0300

    adicionado projeto métodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(13);
+
+        System.out.println("TV Ligada ? "+smartTv.ligada);
+        System.out.println("Canal atual: "+smartTv.canal);
+        System.out.println("Volume atual: "+smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("Novo status -> TV Ligada ? "+smartTv.ligada);
+
+
+        smartTv.desligar();
+
+        System.out.println("Novo status -> TV Ligada ? "+smartTv.ligada);
+
+    }
+}
